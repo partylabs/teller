7aaecd29be7e78b3c2551cd2ddee8d362402127c
Joe Blau <im@joeblau.com>
2023-10-05 07:24:50 -0700

    feat: update ethereum logo

--- src/app/api/token-balances/route.ts
+++ src/app/api/token-balances/route.ts
@@ -29,7 +29,7 @@ export async function POST(request: NextRequest) {
         chainId: mainnet.id,
         address: null,
         ...mainnet.nativeCurrency,
-        logoURI: "https://token.partylabs.org/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2.webp".toLowerCase(),
+        logoURI: "https://token.partylabs.org/0x0000000000000000000000000000000000000000.webp".toLowerCase(),
         publicKey: publicKey,
         value: balance.toString(),
       };

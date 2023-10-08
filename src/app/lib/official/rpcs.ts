183b9e762ceee44d7e6d2ebac4e4a6e51816135d
Joe Blau <im@joeblau.com>
2023-10-07 17:53:57 -0700

    feat: hard code urls

--- src/app/lib/official/rpcs.ts
+++ src/app/lib/official/rpcs.ts
@@ -1,12 +1,12 @@
 import { arbitrum, base, bsc, mainnet, optimism, polygon, pulsechain } from "viem/chains";
 
 export const RPCS = {
-  [mainnet.id]: process.env.MAINNET_RPC_URL,
+  [mainnet.id]: "https://eth.llamarpc.com",
   //   "10": optimism,
   //   "56": bsc,
   //   "137": polygon,
   // [zkSync.id]: zkSync,
-  [pulsechain.id]: process.env.PULSECHAIN_RPC_URL,
+  [pulsechain.id]: "https://rpc.pulsechain.com",
   //   "8_453": base,
   //   "42_161": arbitrum,
 };

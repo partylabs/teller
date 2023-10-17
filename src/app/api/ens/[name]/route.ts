433dcfe83bea6348ac3ae169d9748898a6a25458
Joe Blau <im@joeblau.com>
2023-10-17 11:24:38 -0700

    feat: add ens check

---
+++ src/app/api/ens/[name]/route.ts
@@ -0,0 +1,32 @@
+import { RPCS } from "@/app/lib/official/rpcs";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { createPublicClient, getAddress, http, isAddress } from "viem";
+import { mainnet } from "viem/chains";
+import { normalize } from "viem/ens";
+
+export async function GET(request: NextRequest) {
+  const name = request.nextUrl.pathname.split("/").pop() as string;
+
+  let providerUrl = RPCS[mainnet.id as unknown as keyof typeof RPCS];
+
+  const client = createPublicClient({
+    chain: mainnet,
+    transport: http(providerUrl),
+  });
+
+  try {
+    const ensAddress = await client.getEnsAddress({
+      name: normalize(name),
+    });
+
+    if (ensAddress == null) {
+      return NextResponse.json({ error: "Not found" }, { status: 404 });
+    } else {
+      return NextResponse.json({ ensAddress: ensAddress }, { status: 200 });
+    }
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: "Internal server error" }, { status: 500 });
+  }
+}

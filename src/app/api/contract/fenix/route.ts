c343f8dd797e93c6aca33ff5c60bba12420cd11c
Joe Blau <im@joeblau.com>
2023-10-23 16:23:44 -0700

    feat: add fenix contrat pasring

--- src/app/api/contract/fenix/route.ts
+++ src/app/api/contract/fenix/route.ts
@@ -1,6 +1,77 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { createPublicClient, getAddress, http, parseAbi } from "viem";
+import { mainnet } from "viem/chains";
+import FENIX_ABI from "@/app/models/abi/fenix.json";
+
+(BigInt.prototype as any).toJSON = function () {
+  return this.toString();
+};
 
 export async function POST(request: NextRequest) {
-  return NextResponse.json([], { status: 200 });
+  const client = createPublicClient({
+    chain: mainnet,
+    transport: http(),
+  });
+
+  const { publicKeys } = await request.json();
+
+  const fenixContract = {
+    address: getAddress("0xC3e8abfA04B0EC442c2A4D65699a40F7FcEd8055"),
+    abi: FENIX_ABI,
+  };
+
+  const fenixCountsContracts = publicKeys.map((publicKey: string) => {
+    return {
+      ...fenixContract,
+      functionName: "stakeCount",
+      args: [getAddress(publicKey)],
+    };
+  });
+
+  const fenixStakeCounts = await client.multicall({
+    contracts: fenixCountsContracts,
+  });
+
+  const fenixStakesContracts = publicKeys.flatMap((publicKey: string, index: number) => {
+    const count = fenixStakeCounts[index].result as bigint;
+
+    if (count === BigInt(0)) {
+      return;
+    }
+
+    let fenixStakesContracts = [];
+    for (let i = 0; i < count; i++) {
+      fenixStakesContracts.push({
+        publicKey: publicKey,
+        stakeId: i,
+        fenixStakesContracts: {
+          ...fenixContract,
+          functionName: "stakeFor",
+          args: [getAddress(publicKey), i],
+        },
+      });
+    }
+    return fenixStakesContracts;
+  });
+
+  // console.log(fenixStakesContracts);
+
+  const fenixStakes = await client.multicall({
+    contracts: fenixStakesContracts.map((contract: any) => contract.fenixStakesContracts).flat(),
+  });
+
+  let results = fenixStakesContracts.flatMap((contract: any, index: number) => {
+    const stake = fenixStakes[index].result;
+
+    const publicKey = contract.publicKey;
+    const stakeId = contract.stakeId;
+    return {
+      publicKey: publicKey,
+      stakeId: stakeId,
+      stake: stake,
+    };
+  });
+
+  return NextResponse.json(results, { status: 200 });
 }
--- src/app/api/contract/xen-crypto/route.ts
+++ src/app/api/contract/xen-crypto/route.ts
@@ -1,39 +1,61 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { createPublicClient, getAddress, http } from "viem";
+import { createPublicClient, getAddress, http, parseAbi } from "viem";
 import { mainnet } from "viem/chains";
-import XENCryptoABI from "@/app/models/abi/xen-crypto.json";
+import XEN_CRYPTO_ABI from "@/app/models/abi/xen-crypto.json";
 
 export async function POST(request: NextRequest) {
   // const XEN_CRYPTO_ABI = await fetch("https://contracts.partylabs.org/xen-crypto.json");
 
-  const xenCryptoContract = {
-    address: getAddress("E39061434BAbCFC05599a6Fb861434BAbCFC05599a6Fb8"),
-    abi: XENCryptoABI,
-  };
+  // const xenCryptoContract = {
+  //   address: getAddress("0x06450dEe7FD2Fb8E39061434BAbCFC05599a6Fb8"),
+  //   abi: XEN_CRYPTO_ABI,
+  // };
 
   const client = createPublicClient({
     chain: mainnet,
     transport: http(),
   });
 
-  // const data = await request.json();
-  // const tokens = data["1"];
+  // const { publicKeys } = await request.json();
+  // const xenCryptoContracts = publicKeys.map((publicKey: string) => {
+  //   const readMint = {
+  //     ...xenCryptoContract,
+  //     functionName: "getUserMint",
+  //     account: getAddress("0xBFBE733d560bc0a4E5502587853f92b3aE83344F"),
+  //   };
+  //   return readMint;
+  //   // const readStake = {
+  //   //   ...xenCryptoContract,
+  //   //   functionName: "getUserStake",
+  //   //   account: getAddress(publicKey),
+  //   // };
+  //   // return [readMint, readStake];
+  // });
+  // console.log(xenCryptoContracts);
 
-  const readMint = {
-    ...xenCryptoContract,
-    functionName: "getUserMint",
-    account: getAddress("0xBFBE733d560bc0a4E5502587853f92b3aE83344F"),
-  };
+  // console.log(XEN_CRYPTO_ABI);
 
-  const readStake = {
-    ...xenCryptoContract,
-    functionName: "getUserStake",
-    account: getAddress("0xBFBE733d560bc0a4E5502587853f92b3aE83344F"),
-  };
+  // const results = await client.multicall({
+  //   contracts: [
+  //     {
+  //       address: getAddress("0x06450dEe7FD2Fb8E39061434BAbCFC05599a6Fb8"),
+  //       abi: XEN_CRYPTO_ABI,
+  //       functionName: "getUserMint",
+  //       account: getAddress("0xBFBE733d560bc0a4E5502587853f92b3aE83344F"),
+  //     },
+  //   ],
+  //   account: getAddress("0xBFBE733d560bc0a4E5502587853f92b3aE83344F"),
+  // });
+  // console.log(results);
 
-  const data = await client.readContract(readMint);
+  // const readMint = {
+  //   ...xenCryptoContract,
+  //   functionName: "getUserMint",
+  //   account: getAddress("0xBFBE733d560bc0a4E5502587853f92b3aE83344F"),
+  // };
+  // const data = await client.readContract(readMint);
 
-  console.log(data);
+  // console.log(data);
   return NextResponse.json([], { status: 200 });
 }

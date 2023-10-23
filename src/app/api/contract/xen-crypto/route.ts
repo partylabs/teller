dd7cf18d1a9ba394385fcf31a1317d5b15cc68ed
Joe Blau <im@joeblau.com>
2023-10-22 20:49:27 -0700

    feat: add xen contract calls

--- src/app/api/contract/xen-crypto/route.ts
+++ src/app/api/contract/xen-crypto/route.ts
@@ -2,13 +2,14 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { createPublicClient, getAddress, http } from "viem";
 import { mainnet } from "viem/chains";
+import XENCryptoABI from "@/app/models/abi/xen-crypto.json";
 
 export async function POST(request: NextRequest) {
-  const XEN_CRYPTO_ABI = await fetch("https://contracts.partylabs.org/xen-crypto.json");
+  // const XEN_CRYPTO_ABI = await fetch("https://contracts.partylabs.org/xen-crypto.json");
 
   const xenCryptoContract = {
-    address: "0x06450dEe7FD2Fb8E39061434BAbCFC05599a6Fb8",
-    abi: XEN_CRYPTO_ABI,
+    address: getAddress("E39061434BAbCFC05599a6Fb861434BAbCFC05599a6Fb8"),
+    abi: XENCryptoABI,
   };
 
   const client = createPublicClient({
@@ -16,8 +17,23 @@ export async function POST(request: NextRequest) {
     transport: http(),
   });
 
-  const data = await request.json();
-  const tokens = data["1"];
+  // const data = await request.json();
+  // const tokens = data["1"];
 
+  const readMint = {
+    ...xenCryptoContract,
+    functionName: "getUserMint",
+    account: getAddress("0xBFBE733d560bc0a4E5502587853f92b3aE83344F"),
+  };
+
+  const readStake = {
+    ...xenCryptoContract,
+    functionName: "getUserStake",
+    account: getAddress("0xBFBE733d560bc0a4E5502587853f92b3aE83344F"),
+  };
+
+  const data = await client.readContract(readMint);
+
+  console.log(data);
   return NextResponse.json([], { status: 200 });
 }
